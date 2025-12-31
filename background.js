23ae1f5866659d7de3fe4b1303f4f4c2fe4a501c
Tom-rior <boxer-roast-hungry@duck.com>
2025-12-31 15:38:20 +0530

    Fix "Receiving end does not exist" error by checking tab status and handling promise rejection

--- background.js
+++ background.js
@@ -1,11 +1,18 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-  });
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  // Only send the message if the updated tab is fully loaded
+  if (changeInfo.status === "complete" && tab.url && tab.url.includes("youtube.com/watch")) {
+    const queryParameters = tab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      videoId: urlParameters.get("v"),
+    }).catch((error) => {
+        // Silently ignore "Receiving end does not exist" errors
+        // This happens if the content script hasn't loaded yet or the tab was closed
+        if (chrome.runtime.lastError) {
+            // Do nothing
+        }
+    });
+  }
+});
